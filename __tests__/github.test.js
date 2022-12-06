34057a594bd8e334d2ec539f29a2f59206e00195
Dylan <dylflanagan@gmail.com>
2022-12-06 09:51:52 -0800

    DELETE /api/v1/github signs out a user - TEST PASSED

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -10,14 +10,14 @@ describe('github auth', () => {
     return setup(pool);
   });
 
-  it('/api/v1/github/login should redirect to the github oauth page', async () => {
+  it('GET /api/v1/github/login should redirect to the github oauth page', async () => {
     const res = await request(app).get('/api/v1/github/login');
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
   });
 
-  it('/api/v1/github/callback should login users and redirect to dashboard', async () => {
+  it('GET /api/v1/github/callback should login users and redirect to dashboard', async () => {
     const res = await request
       .agent(app)
       .get('/api/v1/github/callback?code=42')
@@ -30,6 +30,10 @@ describe('github auth', () => {
       iat: expect.any(Number),
       exp: expect.any(Number),
     });
+  });
+  it('DELETE /api/v1/github signs out a user', async () => {
+    const res = await request(app).get('/api/v1/github/dashboard');
+    expect(res.status).toBe(401);
   });
 });
 
